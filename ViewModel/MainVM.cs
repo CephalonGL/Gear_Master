b506873be25e7df20ae6725902bb1d6c6efa210f
Gregory Lubov <glubov@yandex.ru>
2023-11-18 23:54:48 +0700

    style(MainVM): исправлено форматирование.

--- ViewModel/MainVM.cs
+++ ViewModel/MainVM.cs
@@ -1,32 +1,35 @@
-﻿using System.Collections.Generic;
-using Model;
-using CommunityToolkit.Mvvm;
-using CommunityToolkit.Mvvm.Input;
-
-namespace ViewModel
+﻿namespace ViewModel
 {
+    using CommunityToolkit.Mvvm.Input;
+    using Model;
+
     /// <summary>
     /// Главный класс ViewModel. Хранит информацию о всём проекте.
     /// </summary>
     public class MainVM
     {
         /// <summary>
-        /// Хранит проект.
-        /// </summary>
-        public Project Project { get; private set; }
-
-        /// <summary>
         /// Конструктро без параметров.
         /// </summary>
         private MainVM()
         {
             Project = new Project();
-            BuildCommand = new RelayCommand(() => Project.Build(CrossValidator.AssertCorrect(Project.Parameters)));
+
+            BuildCommand =
+                new RelayCommand(() =>
+                                     Project.Build(CrossValidator
+                                                      .AssertCorrect(Project.Parameters)));
         }
-        
+
+        /// <summary>
+        /// Хранит проект.
+        /// </summary>
+        public Project Project { get; private set; }
+
+
         /// <summary>
         /// Команда построения модели в САПР.
         /// </summary>
-        public RelayCommand BuildCommand;
+        public RelayCommand BuildCommand { get; private set; }
     }
 }
