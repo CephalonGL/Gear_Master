33c409b18f9d454c98982ca9ab429e606c19d09e
Gregory Lubov <glubov@yandex.ru>
2023-11-20 14:03:57 +0700

    feature(sln): добавлен файл коннектора к Autodesk AutoCAD.

---
+++ AutoCADConnector/Connector.cs
@@ -0,0 +1,18 @@
+﻿namespace AutoCADConnector
+{
+    using Autodesk.AutoCAD.Runtime;
+    using GearMaster;
+
+    public class Connector
+    {
+        /// <summary>
+        /// Запускает плагин из AutoCAD.
+        /// </summary>
+        [CommandMethod("GearMaster")]
+        public void StartGearMaster()
+        {
+            var window = new MainWindow();
+            window.Show();
+        }
+    }
+}
