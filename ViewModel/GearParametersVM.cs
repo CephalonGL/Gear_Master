670ab2257be57049f4b4acf8de4ed856d3b0a2ca
Gregory Lubov <glubov@yandex.ru>
2023-11-20 18:05:05 +0700

    feature(GearParametersVM): создан класс для отображения параметров шестерни на уровне VM.

---
+++ ViewModel/GearParametersVM.cs
@@ -0,0 +1,24 @@
+﻿namespace ViewModel
+{
+    using Model;
+
+    /// <summary>
+    /// Представление для параметров шестерни в VM.
+    /// </summary>
+    public class GearParametersVM
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="parameters">Параметры шестерни.</param>
+        public GearParametersVM(GearParameters parameters)
+        {
+            Parameters = parameters;
+        }
+
+        /// <summary>
+        /// Хранит парамеры шестерни.
+        /// </summary>
+        public GearParameters Parameters { get; private set; }
+    }
+}
