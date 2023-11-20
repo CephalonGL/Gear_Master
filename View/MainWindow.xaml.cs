1462a9da3fb1eedadb34ffce3c3ea10c2bfea370
Gregory Lubov <glubov@yandex.ru>
2023-11-20 14:03:20 +0700

    style(MainWindow): добавлены XML-комментарии.

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
 ﻿namespace GearMaster
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml.
     /// </summary>
     public partial class MainWindow
     {
+        /// <summary>
+        /// Инициализирует главное окно программы.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
